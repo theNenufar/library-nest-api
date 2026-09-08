603f5db86d1ed1b43eb6b4269dac7fd84f86b0a7
theNenufar <nenufarmarinidias@gmail.com>
2026-09-08 00:00:52 -0300

    feat: add basic angular component to consume api

--- frontend-app/src/app/app.ts
+++ frontend-app/src/app/app.ts
@@ -1,11 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { User, UserService } from './users/user.service';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend-app');
+export class App implements OnInit {
+
+  users: User[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe({
+      next: users => {
+        console.log('Usuários recebidos:', users);
+        this.users = users;
+      },
+      error: error => {
+        console.error('Erro ao buscar usuários:', error);
+      }
+    });
+  }
 }
---
+++ frontend-app/src/app/users/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = 'http://localhost:3000/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
+}
